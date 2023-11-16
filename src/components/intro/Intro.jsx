296297cfcd5c991f8fed379303dc238e28ae4795
Ariel Cabeza <cervanteslibch@gmail.com>
2023-11-16 01:21:02 -0300

    intro 2nd part

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,25 +1,40 @@
+import { useEffect, useRef } from 'react';
+import { init } from 'ityped'
 import './intro.scss';
 
 const Intro = () => {
-  return (
-    <div className='intro' id="intro">
-      <div className="left">
-        <div className="img-container">
-          <img src="assets/man.png" alt="" />
+
+    
+    const textRef = useRef();
+    useEffect(() => {
+
+        init(textRef.current, { 
+            showCursor: true, 
+            backDelay: 1500,
+            strings: ['Developer', 'M.E.R.N', 'React', 'Node Js', 'Mongo DB' ] 
+        })
+    }, [])
+
+
+    return (
+        <div className='intro' id="intro">
+            <div className="left">
+                <div className="img-container">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Saludos! Soy</h2>
+                    <h1>Ariel Cabeza</h1>
+                    <h3>Fullstack <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
-      </div>
-      <div className="right">
-        <div className="wrapper">
-          <h2>Saludos! Soy</h2>
-          <h1>Ariel Cabeza</h1>
-          <h3>Fullstack <span></span></h3>
-        </div>
-        <a href="#portfolio">
-          <img src="assets/down.png" alt="" />
-        </a>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Intro
