fb758ea9af719b46f8546dd082514398141dcbd4
Ariel Cabeza <cervanteslibch@gmail.com>
2023-11-16 00:54:25 -0300

    intro first part

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,21 @@ import './intro.scss';
 const Intro = () => {
   return (
     <div className='intro' id="intro">
+      <div className="left">
+        <div className="img-container">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Saludos! Soy</h2>
+          <h1>Ariel Cabeza</h1>
+          <h3>Fullstack <span></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
